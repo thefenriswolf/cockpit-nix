bc09a0f3e4874e9c3d6c25c1c1ead4966250cffa
thefenriswolf <stefan.rohrbacher97@gmail.com>
2020-09-19 18:15:58 +0200

    c

--- default.nix
+++ default.nix
@@ -1,26 +1,48 @@
-{ fetchFromGitHub, lib, fetchzip, stdenv, packagekit, pkgconfig, intltool, perl
-, perlPackages, gobject-introspection, json-glib, glib-networking, xmlto
-, networkmanager, libxslt, gtk-doc, systemd, libssh, krb5, pam_krb5, linux-pam
-, python, pythonPackages, nodejs, nodePackages, automake }:
+{ lib
+, fetchzip
+, stdenv
+, packagekit
+, pkgconfig
+, intltool
+, perl
+, perlPackages
+, gobject-introspection
+, json-glib
+, glib-networking
+, xmlto
+, networkmanager
+, libxslt
+, gtk-doc
+, systemd
+, libssh
+, krb5
+, pam_krb5
+, linux-pam
+, python
+, pythonPackages
+, nodejs
+, nodePackages
+, automake
+}:
 # I am pretty sure that not all these deps are really needed at compile time but these were taken from an AUR script
 # nixpkgs is missing pcp which cockpit complains about at the configure step
 # atm this does not compile. without pyscss it complains about libpthread not being present, with it it does not compile either 
 
 stdenv.mkDerivation rec {
   pname = "cockpit";
-  version = "225";
+  version = "226";
 
   src = fetchzip {
     url =
       "https://github.com/cockpit-project/cockpit/releases/download/${version}/cockpit-${version}.tar.xz";
-    sha256 = "1irwnl876ryzfqp6vwnfl2amffy80df50k1z5ih1ar9225p5paf3";
+    sha256 = "0yf80k25gdhsrcgnhyg60acfbn3h86c4jqshwsl40cxmpwns5844";
   };
 
   #  src = fetchFromGitHub { # autoreconfigure complains about not being in a git repo if the source was not pulled from the tgz archive
-  #    owner = "catern";
+  #    owner = "cockpit-project";
   #    repo = "cockpit";
-  #    rev = "e0e747d4ffc24ce90ac218c0f944bbf73f3830d1";
-  #    sha256 = "1lqd99waxw2b572hnqjmg9ai6hyjk8kab4ja7im3aw86cd41nvsa";
+  #    rev = "d03cee1c4fde351b76f751d9122a2ab3370afa55";
+  #    sha256 = "1ayidnf4fiq1bxzi66cvw80djg7vnyfhnz60rbcnr9b7dfgs8sc9";
   #  };
 
   nativeBuildInputs = [ nodejs automake pkgconfig python ];
